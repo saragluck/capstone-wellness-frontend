21288ab60544b1cfcf8ea25151732cb7b87e7a41
Sara Gluck <srfgglck@gmail.com>
2023-01-04 11:39:35 -0600

    adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { LandingPage } from "./LandingPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <LandingPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
