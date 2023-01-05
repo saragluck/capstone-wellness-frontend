e8f636a788937be52f80f605d7c18e5897eb0ef3
Sara Gluck <srfgglck@gmail.com>
2023-01-05 15:46:44 -0600

    makes form in goal modal

--- src/GoalsShow.jsx
+++ src/GoalsShow.jsx
@@ -4,6 +4,15 @@ export function GoalsShow(props) {
       <h1>Goal</h1>
       <h2>Goal: {props.goal.goal}</h2>
       <h2>Category: {props.goal.category}</h2>
+      <form>
+        <div>
+          Goal: <input defaultValue={props.goal.goal} name="goal" type="text" />
+        </div>
+        <div>
+          Category: <input defaultValue={props.goal.category} name="category" type="text" />
+        </div>
+        <button type="submit">Update</button>
+      </form>
     </div>
   );
 }
