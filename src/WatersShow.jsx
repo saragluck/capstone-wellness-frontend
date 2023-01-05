63c4539a501a68dffed518330a6e90ab97bf44af
Sara Gluck <srfgglck@gmail.com>
2023-01-05 14:57:13 -0600

    makes handle submit so that the water can be updated

--- src/WatersShow.jsx
+++ src/WatersShow.jsx
@@ -1,11 +1,17 @@
 export function WatersShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onUpdateWater(props.water.id, params, () evemt.target.reset());
+  };
+
   return (
     <div>
       <h1>Water Info</h1>
       <h2>Amount: {props.water.amount}</h2>
       <h2>Time: {props.water.time} </h2>
       <h2>Date: {props.water.date}</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           Amount: <input defaultValue={props.water.amount} name="amount" type="text" />
         </div>
