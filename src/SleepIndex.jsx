d70546d21fb412c743a4c70fd7ecd344e71e25c4
Sara Gluck <srfgglck@gmail.com>
2023-01-05 22:11:51 -0600

    makes the sleep index friendlier

--- src/SleepIndex.jsx
+++ src/SleepIndex.jsx
@@ -1,3 +1,5 @@
+import moment from "moment";
+
 export function SleepIndex(props) {
   return (
     <div>
@@ -5,7 +7,8 @@ export function SleepIndex(props) {
       {props.sleeps.map((sleep) => (
         <div key={sleep.id}>
           <h2>
-            I slept from {sleep.asleep} to {sleep.awake} on {sleep.date}.
+            I slept from {moment(sleep.asleep).format("hh:mm")} to {moment(sleep.awake).format("hh:mm")} on{" "}
+            {moment(sleep.date).format("MMMM Do, YYYY")}.
           </h2>
           <button onClick={() => props.onShowSleep(sleep)}>Edit</button>
         </div>
