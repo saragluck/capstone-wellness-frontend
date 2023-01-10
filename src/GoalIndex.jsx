07a5ada4bf535f00b0a2c953f100bfce95b37aec
Sara Gluck <srfgglck@gmail.com>
2023-01-10 15:49:26 -0600

    fixes goals so you dont drink hours of sleep

--- src/GoalIndex.jsx
+++ src/GoalIndex.jsx
@@ -1,15 +1,67 @@
+import Carousel from "react-bootstrap/Carousel";
+
 export function GoalIndex(props) {
+  // const goals = [
+  //   {props.goals.map((goal) => (
+  //     <div key={goal.id}>
+  //       <h2>
+  //         I want to drink {goal.goal} of {goal.category}.
+  //       </h2>
+  //       <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>Edit</button>
+  //     </div>
+  //   ))}
+  // ]
   return (
     <div>
+      <h1>Goals</h1>
+      <div className="card-group">
+        <div className="card">
+          <div className="card-body">
+            {props.goals.map((goal) => (
+              <div key={goal.id}>
+                <h2>
+                  I want to {goal.goal} of {goal.category}.
+                </h2>
+                <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>
+                  Edit
+                </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <h1>Goals</h1>
       {props.goals.map((goal) => (
         <div key={goal.id}>
           <h2>
             I want to drink {goal.goal} of {goal.category}.
           </h2>
-          <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>Edit</button>
+          <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>
+            Edit
+          </button>
         </div>
       ))}
+      <Carousel>
+        {/* {props.goals.map((goal) => (
+          <div key={goal.id}>
+            <h2>
+              I want to drink {goal.goal} of {goal.category}.
+            </h2>
+            <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>
+              Edit
+            </button>
+          </div>
+        ))} */}
+        {props.children}
+      </Carousel>
+      {/* {props.goals.map((goal) => (
+        <div key={goal.id}>
+          <h2>
+            I want to drink {goal.goal} of {goal.category}.
+          </h2>
+          <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowGoal(goal)}>Edit</button>
+        </div>
+      ))} */}
     </div>
   );
 }
