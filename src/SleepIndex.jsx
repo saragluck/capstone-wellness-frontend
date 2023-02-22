19d9bff71795b7916c6364e38b8d647a8bf27618
saragluck <srfgglck@gmail.com>
2023-02-21 19:20:46 -0600

    fixes background to work on more devices, and be responsive

--- src/SleepIndex.jsx
+++ src/SleepIndex.jsx
@@ -4,6 +4,9 @@ import "moment-timezone";
 // an issue here: if you go to sleep in the pm and wake up the next am, you have a negative duration.
 // yay, I fixed it! I made it be the absolute value of the duration
 export function SleepIndex(props) {
+  
+
+
   return (
     <div>
       <h1>Sleep</h1>
@@ -15,12 +18,13 @@ export function SleepIndex(props) {
           </h2>
           <h3>
             Duration:{" "}
-            {Math.abs(
+            {(24-Math.abs(
               moment
                 .duration(moment(sleep.awake, "YYYY/MM/DD hh:mm").diff(moment(sleep.asleep, "YYYY/MM/DD hh:mm")))
                 .asHours()
-            ).toFixed(2)}{" "}
-            hours
+            )).toFixed(2)} {" "}
+          hours
+
           </h3>
           <button className="btn btn-outline-dark rounded-pill" onClick={() => props.onShowSleep(sleep)}>
             Edit
