d3762191be7b8b0e320755c963f13ddbabdeffba
saragluck <srfgglck@gmail.com>
2023-10-24 13:51:22 -0500

    changes sleep graph prop

--- src/SleepGraph.jsx
+++ src/SleepGraph.jsx
@@ -5,7 +5,6 @@ import { useState, useEffect } from "react";
 const SleepGraph = () => {
   const [sleepData, setSleepData] = useState([]);
   const jwt = localStorage.getItem("jwt");
-  const [dataChange, setDataChange] = useState(false);
 
   
   useEffect(() => {
@@ -21,7 +20,7 @@ const SleepGraph = () => {
     }; // function to get the data associated with the current user (using JWT authorization)
 
     fetchData();
-  }, [dataChange]);
+  }, []);
 
 
   const labels = sleepData.map((data) => data.date); // sets the labels of the graph x axis to be the dates
@@ -49,5 +48,53 @@ const SleepGraph = () => {
 };
 
 export default SleepGraph;
+
+// import { Line } from 'react-chartjs-2';
+// import React, { useEffect, useState } from 'react';
+
+// const SleepGraph = ({ sleepData }) => {
+//   // Initialize a local state variable to manage the data for the graph
+//   const [data, setData] = useState({
+//     labels: [],
+//     datasets: [
+//       {
+//         label: 'Sleep Duration',
+//         backgroundColor: 'rgb(255, 99, 132)',
+//         borderColor: 'rgb(132, 99, 255)',
+//         data: [],
+//         fill: false,
+//         tension: 0.1,
+//       },
+//     ],
+//   });
+
+//   // Use the useEffect hook to listen for changes in the sleepData prop
+//   useEffect(() => {
+//     // Extract labels and duration data from the sleepData prop
+//     const labels = sleepData.map((data) => data.date);
+//     const durationData = sleepData.map((data) => data.duration);
+
+//     // Update the local data state with the new labels and duration data
+//     setData((sleepData) => ({
+//       ...sleepData,
+//       labels: labels,
+//       datasets: [
+//         {
+//           ...sleepData.datasets[0], // Copy the previous dataset
+//           data: durationData,     // Update the data with new duration data
+//         },
+//       ],
+//     }));
+//   }, [sleepData]); // This useEffect will run whenever sleepData changes
+
+//   return (
+//     <div>
+//       <Line data={data} /> 
+//     </div>
+//   );
+// };
+
+// export default SleepGraph;
+
 
 
