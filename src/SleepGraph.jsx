5da538d72772d967f7165ba83ff52163ce5791b7
saragluck <srfgglck@gmail.com>
2023-10-24 13:58:19 -0500

    deletes extra stuff

--- src/SleepGraph.jsx
+++ src/SleepGraph.jsx
@@ -49,52 +49,5 @@ const SleepGraph = () => {
 
 export default SleepGraph;
 
-// import { Line } from 'react-chartjs-2';
-// import React, { useEffect, useState } from 'react';
-
-// const SleepGraph = ({ sleepData }) => {
-//   // Initialize a local state variable to manage the data for the graph
-//   const [data, setData] = useState({
-//     labels: [],
-//     datasets: [
-//       {
-//         label: 'Sleep Duration',
-//         backgroundColor: 'rgb(255, 99, 132)',
-//         borderColor: 'rgb(132, 99, 255)',
-//         data: [],
-//         fill: false,
-//         tension: 0.1,
-//       },
-//     ],
-//   });
-
-//   // Use the useEffect hook to listen for changes in the sleepData prop
-//   useEffect(() => {
-//     // Extract labels and duration data from the sleepData prop
-//     const labels = sleepData.map((data) => data.date);
-//     const durationData = sleepData.map((data) => data.duration);
-
-//     // Update the local data state with the new labels and duration data
-//     setData((sleepData) => ({
-//       ...sleepData,
-//       labels: labels,
-//       datasets: [
-//         {
-//           ...sleepData.datasets[0], // Copy the previous dataset
-//           data: durationData,     // Update the data with new duration data
-//         },
-//       ],
-//     }));
-//   }, [sleepData]); // This useEffect will run whenever sleepData changes
-
-//   return (
-//     <div>
-//       <Line data={data} /> 
-//     </div>
-//   );
-// };
-
-// export default SleepGraph;
-
 
 
