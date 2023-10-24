2c671cd18a54be9f1d803eb1ff3e876aa2255bb7
saragluck <srfgglck@gmail.com>
2023-10-24 14:02:27 -0500

    Adds action cable and refetches data on change

--- src/SleepGraph.jsx
+++ src/SleepGraph.jsx
@@ -1,6 +1,8 @@
 import { Line } from "react-chartjs-2";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import ActionCable from 'actioncable';
+
 
 const SleepGraph = () => {
   const [sleepData, setSleepData] = useState([]);
@@ -18,9 +20,26 @@ const SleepGraph = () => {
       const data = response.data;
       setSleepData(data);
     }; // function to get the data associated with the current user (using JWT authorization)
+    
+  const cable = ActionCable.createConsumer("ws://localhost:3000/cable");
+  const channel = cable.subscriptions.create({ channel: "SleepsChannel" }, {
+    received: (data) => {
+      // Handle the received message from the backend
+      // Whenever a notification of an event is received, refetch data
+      if (data.event === 'update') {
+        fetchData();
+      }
+    }
+  });
 
-    fetchData();
-  }, []);
+  // Fetch initial data when the component mounts
+  fetchData();
+
+  // Return a cleanup function to unsubscribe from the channel when the component unmounts
+  return () => {
+    channel.unsubscribe();
+  };
+}, []);
 
 
   const labels = sleepData.map((data) => data.date); // sets the labels of the graph x axis to be the dates
